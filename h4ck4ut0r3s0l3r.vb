1b0c6ef93dc5685386d10c3dc5336099d682c9e4
water monke <87366457+stopsimping@users.noreply.github.com>
2022-04-27 12:14:22 +0200

    bruh

---
+++ h4ck4ut0r3s0l3r.vb
@@ -0,0 +1,21 @@
+﻿Public Module h4ck4ut0r3s0l3r
+    Public Sub autoResolve(enable As Boolean)
+        If (enable) Then
+            For Each cell As Cell In Game.p_cases.Controls
+                If (cell.getValue <> "M") Then
+                    Reveal(cell, Game.p_cases.Controls.IndexOf(cell))
+                End If
+            Next
+        End If
+    End Sub
+
+    Friend Sub trollAlwaysLoose(enable As Boolean)
+        If (enable) Then
+            For Each cell As Cell In Game.p_cases.Controls
+                If (cell.getValue = "M") Then
+                    Reveal(cell, Game.p_cases.Controls.IndexOf(cell))
+                End If
+            Next
+        End If
+    End Sub
+End Module
